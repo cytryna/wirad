7e0f07812a7b0c2a2c79616d3d3707dd636de424
radek <radoslaw.wichrowski@gmail.com>
2017-12-10 12:12:53 +0100

    Nieudane próby zaimplementowania profili w projekcie

--- src/main/java/com/javacodegeeks/spring/jpa/JpaConfig.java
+++ src/main/java/com/javacodegeeks/spring/jpa/JpaConfig.java
@@ -8,26 +8,53 @@ import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.datasource.DataSourceUtils;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+import org.springframework.jndi.JndiObjectFactoryBean;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
+//@Profile("dev")
+//@ActiveProfiles("dev")
 @EnableTransactionManagement
 public class JpaConfig {
 
+//	@Bean(destroyMethod = "shutdown")
+//	@Profile("dev")
+//	public DataSource embeddedDataSource() {
+//		EmbeddedDatabase build = new EmbeddedDatabaseBuilder()
+//				.setType(EmbeddedDatabaseType.H2)
+//				.addScript("classpath:schema.sql")
+//				.addScript("classpath:test-data.sql")
+//				.build();
+//		return build;
+//	}
+
+//	@Bean
+//	@Profile("prod")
+//	public DataSource jndiDataSource() {
+//		JndiObjectFactoryBean jndiObjectFactoryBean = new JndiObjectFactoryBean();
+//		jndiObjectFactoryBean.setJndiName("jdbc/myDS");
+//		jndiObjectFactoryBean.setResourceRef(true);
+//		jndiObjectFactoryBean.setProxyInterface(javax.sql.DataSource.class);
+//		return (DataSource) jndiObjectFactoryBean.getObject();
+//	}
 	/**
 	 * Bootstraps an in-memory HSQL database.
 	 */
 	@Bean
+//	@Profile("dev")
 	public DataSource dataSource() {
 		EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
 		return builder.setType(EmbeddedDatabaseType.HSQL).build();
